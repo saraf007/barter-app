0cdf7e4b84b7456d86558dec5dbe40f521d9c982
Vikas Saraf <saraf.vikas5@gmail.com>
2021-02-07 20:29:02 +0530

    added several components

--- barter-app/src/app/app.module.ts
+++ barter-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    CarouselComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
